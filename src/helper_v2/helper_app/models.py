a830bff94087c1851dff29bc1d1f2ef9840994b1
vic-map <83211386+vic-map@users.noreply.github.com>
2022-01-28 15:10:45 +0200

    created classes(as tables to DB): Contacts, NoteTags, Notes, FileTypes, Files

--- src/helper_v2/helper_app/models.py
+++ src/helper_v2/helper_app/models.py
@@ -2,4 +2,51 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.timezone import now
 
-# Create your models here.
+
+class Contacts(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=False, blank=False,)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=100)
+    email = models.EmailField(max_length=50)
+    b_day = models.DateField(default=now().date(), null=True)
+    is_favorite = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} {self.phone_number}'
+
+
+class NoteTags(models.Model):
+    note_tag = models.CharField(
+        max_length=20, default='just note', null=False, blank=False,)
+
+
+class Notes(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=False, blank=false, )
+    note_name = models.CharField(max_length=100, null=False, blank=False)
+    note_path = models.CharField(max_length=300)
+    note_tags = models.ForeignKey(
+        NoteTags, on_delete=models.CASCADE, null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.note_name} {self.note_tags}'
+
+
+class FileTypes():
+    file_type = models.CharField(max_length=20)
+    file_extention = models.CharField(max_length=5)
+
+
+class Files(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=False, blank=False)
+    file_name = models.CharField()
+    file_type = models.ForeignKey(
+        FileTypes, on_delete=models.CASCADE, null=False, blank=False, )
+    file_date = models.DateField()
+    file_path = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f'{self.file_name} {self.file_type} {self.file_date}'
