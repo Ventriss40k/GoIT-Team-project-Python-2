1b0b78abb9b3aa746b3941a2e0bdb7370104d5b6
vic-map <83211386+vic-map@users.noreply.github.com>
2022-01-29 20:08:08 +0200

    added view for contact

--- src/helper_v2/helper_app/views.py
+++ src/helper_v2/helper_app/views.py
@@ -1,30 +1,58 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
+from django.views.generic.list import ListView
 from django.http import HttpResponseRedirect, HttpResponse
 from django.utils.text import slugify
+from .models import Contacts, Notes, NoteTags, Files, FileTypes
 # import json
-# from .models import Project, Category, Expense
+
 # from .forms import ExpenseForm, FilterForm
 
+from django.db import transaction
 
 
 class HomeView(TemplateView):
-    template_name = "assistant/home.html"  
+    template_name = "assistant/home.html"
 
 
-class ContactsView(TemplateView):
-    template_name = "assistant/contacts.html"    
-    
+class ContactsView(ListView):
+    template_name = "assistant/contacts.html"
+    model = Contacts
+    context_object_name = 'contacts'
+
+    def get_context_data(self, *kwargs):
+        context = super().get_context_data(*kwargs)
+        context['contacts'] = context['contacts']
+        context['count'] = context['contacts'].count()
+
+        first_name_input = self.request.GET.get('serch-by-name') or ''
+        if first_name_input:
+            context['contacts'] = context['contacts'].filter(
+                first_name=first_name_input)
+
+        last_name_input = self.request.GET.get('serch-by-last-name') or ''
+        if last_name_input:
+            context['contacts'] = context['contacts'].filter(
+                last_name=last_name_input)
+
+        phone_input = self.request.GET.get('serch-by-phone-number') or ''
+        if phone_input:
+            context['contacts'] = context['contacts'].filter(
+                phone_number=phone_input)
+
+        email_input = self.request.GET.get('serch-by-email') or ''
+        if email_input:
+            context['contacts'] = context['contacts'].filter(
+                email=email_input)
+
 
 class NotesView(TemplateView):
-    template_name = "assistant/notes.html" 
+    template_name = "assistant/notes.html"
 
 
 class NewsView(TemplateView):
-    template_name = "assistant/news.html" 
+    template_name = "assistant/news.html"
 
 
 class AboutView(TemplateView):
     template_name = "assistant/about_us.html"
-
-  
