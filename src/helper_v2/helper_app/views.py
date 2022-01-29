c0517bd349ded8c1c15f1740db07c7dad0ff736d
vic-map <83211386+vic-map@users.noreply.github.com>
2022-01-29 23:16:54 +0200

    views for login & register added

--- src/helper_v2/helper_app/views.py
+++ src/helper_v2/helper_app/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render, get_object_or_404
+
+
 from django.views.generic import TemplateView
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+from django.views import View
+from django.shortcuts import redirect
+from django.db import transaction
+
 from django.http import HttpResponseRedirect, HttpResponse
 from django.utils.text import slugify
 from .models import Contacts, Notes, NoteTags, Files, FileTypes
@@ -10,7 +22,32 @@ from django.urls import reverse_lazy
 
 # from .forms import ExpenseForm, FilterForm
 
-from django.db import transaction
+
+class CustomLoginView(LoginView):
+    template_name = 'helper_app/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+class RegisterPage(FormView):
+    template_name = 'helper_app/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('home')
+        return super(RegisterPage, self).get(*args, **kwargs)
 
 
 class HomeView(TemplateView):
